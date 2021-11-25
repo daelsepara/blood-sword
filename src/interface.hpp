3a938e8420e3bc4774071c898912e887f23eddc1
Dael Separa <dael.separa@gmail.com>
2021-11-25 14:41:40 +0800

    dropping items

--- src/interface.hpp
+++ src/interface.hpp
@@ -4813,7 +4813,7 @@ namespace Interface
         Graphics::RenderButtons(Renderer, Controls, Current, text_space, border_pts);
     }
 
-    std::vector<Button> EquipmentList(SDL_Window *Window, SDL_Renderer *Renderer, std::vector<Equipment::Base> Equipment, int WindowW, int WindowH, int WindowTextX, int WindowTextY, int Start, int Last, int Limit, SDL_Color Fg, Uint32 Bg, Uint32 Highlight)
+    std::vector<Button> EquipmentList(SDL_Window *Window, SDL_Renderer *Renderer, std::vector<Equipment::Base> &Equipment, int WindowW, int WindowH, int WindowTextX, int WindowTextY, int Start, int Last, int Limit, SDL_Color Fg, Uint32 Bg, Uint32 Highlight)
     {
         auto IconSize = (buttonw + 2 * text_space);
         auto FontSize = TTF_FontHeight(Fonts::Normal);
@@ -4867,6 +4867,82 @@ namespace Interface
         return Controls;
     }
 
+    int Choose(SDL_Window *Window, SDL_Renderer *Renderer, std::vector<Button> &StoryScreen, Party::Base &Party, Story::Base *Story, ScreenDimensions &Screen, SDL_Surface *Text, int Offset, std::vector<Assets::Type> Assets, std::vector<std::string> Captions, const char *Message)
+    {
+        auto Result = -1;
+
+        auto FontSize = TTF_FontHeight(Fonts::Normal);
+        auto WindowW = 3 * SCREEN_WIDTH / 5;
+        auto WindowH = 4 * FontSize + Screen.IconSize;
+        auto WindowX = (SCREEN_WIDTH - WindowW) / 2;
+        auto WindowY = Screen.TextBoxY + (Screen.TextBoxHeight - WindowH) / 2;
+        auto ButtonX = WindowX + 2 * text_space;
+
+        auto Hold = false;
+        auto Selected = false;
+        auto ScrollUp = false;
+        auto ScrollDown = false;
+        auto Current = 0;
+
+        std::vector<Button> Controls = {};
+
+        auto NumControls = 0;
+
+        for (auto i = 0; i < Assets.size(); i++)
+        {
+            Controls.push_back(Button(NumControls, Assets::Get(Assets[NumControls]), NumControls > 0 ? NumControls - 1 : 0, NumControls + 1, NumControls, NumControls, ButtonX + NumControls * (Screen.IconSize + 2 * text_space), WindowY + Screen.IconSize, intBK, Control::Type::CHOICE));
+
+            NumControls++;
+        }
+
+        Controls.push_back(Button(NumControls, Assets::Get(Assets::Type::Back), NumControls > 0 ? NumControls - 1 : 0, NumControls, NumControls, NumControls, ButtonX + NumControls * (Screen.IconSize + 2 * text_space), WindowY + Screen.IconSize, intBK, Control::Type::BACK));
+
+        auto done = false;
+
+        while (!done)
+        {
+            Interface::RenderStoryScreen(Window, Renderer, Party, Story, Screen, StoryScreen, Text, -1, Offset);
+            Graphics::FillRect(Renderer, WindowW, WindowH, WindowX, WindowY, intWH);
+            Graphics::DrawRect(Renderer, WindowW, WindowH, WindowX, WindowY, intGR);
+
+            Graphics::PutText(Renderer, Message, Fonts::Normal, text_space, clrGR, intWH, TTF_STYLE_NORMAL, WindowW - 4 * text_space, TTF_FontHeight(Fonts::Normal), ButtonX - text_space, WindowY + text_space);
+
+            Graphics::RenderButtons(Renderer, Controls, Current, text_space, border_pts);
+
+            if (Current >= 0 && Current < Controls.size())
+            {
+                if (Controls[Current].Type != Control::Type::BACK)
+                {
+                    Interface::RenderChoiceCaption(Renderer, Controls[Current], Captions[Current], clrBK, intWH);
+                }
+                else
+                {
+                    Graphics::RenderCaption(Renderer, Controls[Current], clrBK, intWH);
+                }
+            }
+
+            Input::GetInput(Renderer, Controls, Current, Selected, ScrollUp, ScrollDown, Hold, 50);
+
+            if ((Selected && Current >= 0 && Current < Controls.size()) || ScrollUp || ScrollDown || Hold)
+            {
+                if (Controls[Current].Type == Control::Type::BACK)
+                {
+                    Result = -1;
+
+                    done = true;
+                }
+                else
+                {
+                    Result = Current;
+
+                    done = true;
+                }
+            }
+        }
+
+        return Result;
+    }
+
     void ItemScreen(SDL_Window *Window, SDL_Renderer *Renderer, std::vector<Button> &StoryScreen, Party::Base &Party, Story::Base *Story, ScreenDimensions &Screen, SDL_Surface *Text, int Offset, int Character, Equipment::Mode Mode)
     {
         auto FontSize = TTF_FontHeight(Fonts::Normal);
@@ -4886,7 +4962,7 @@ namespace Interface
         auto ScrollSpeed = 1;
 
         auto ItemOffset = 0;
-        auto Limit = (10 * FontSize) / (2 * FontSize + 2 * text_space);
+        auto Limit = (8 * FontSize) / (2 * FontSize + 2 * text_space);
         auto Last = ItemOffset + Limit;
 
         std::vector<Equipment::Base> &Equipment = Party.Members[Character].Equipment;
@@ -5031,6 +5107,54 @@ namespace Interface
                         Engine::Erase(Selection, Current + ItemOffset);
                     }
                 }
+                else if (Controls[Current].Type == Control::Type::DROP && !Hold)
+                {
+                    if (Equipment.size() > 0 && Selection.size() > 0)
+                    {
+                        std::string Message = "You are about to drop " + (Selection.size() > 1 ? std::string("several items") : "the " + Equipment[Selection[0]].String());
+
+                        std::string Options = Selection.size() > 1 ? "Drop items" : ("Drop the " + Equipment[Selection[0]].String());
+
+                        auto Result = Interface::Choose(Window, Renderer, StoryScreen, Party, Story, Screen, Text, Offset, {Assets::Type::Ok}, {Options.c_str()}, Message.c_str());
+
+                        if (Result == 0)
+                        {
+                            auto New = std::vector<Equipment::Base>();
+
+                            for (auto i = 0; i < Equipment.size(); i++)
+                            {
+                                if (!Engine::InList(Selection, i))
+                                {
+                                    New.push_back(Equipment[i]);
+                                }
+                            }
+
+                            Equipment = New;
+
+                            Selection.clear();
+
+                            if (Equipment.size() == 0)
+                            {
+                                done = true;
+                            }
+                            else
+                            {
+                                ItemOffset = 0;
+
+                                Last = ItemOffset + Limit;
+
+                                if (Last > Equipment.size())
+                                {
+                                    Last = Equipment.size();
+                                }
+
+                                Controls = Interface::EquipmentList(Window, Renderer, Equipment, WindowW, WindowH, ButtonX, TextY + FontSize, ItemOffset, Last, Limit, Fg, Bg, Highlight);
+
+                                Current = -1;
+                            }
+                        }
+                    }
+                }
             }
         }
     }
@@ -5057,6 +5181,8 @@ namespace Interface
 
         auto Controls = std::vector<Button>();
 
+        std::vector<Equipment::Base> &Equipment = Party.Members[Character].Equipment;
+
         Controls.push_back(Button(0, Assets::Get(Assets::Type::Encyclopedia), 0, 1, 0, 0, ButtonX, OffsetY, intBK, Control::Type::INFO));
         Controls.push_back(Button(1, Assets::Get(Assets::Type::Stats), 0, 2, 1, 1, ButtonX + Screen.IconSize, OffsetY, intBK, Control::Type::STATS));
         Controls.push_back(Button(2, Assets::Get(Assets::Type::Ability), 1, 3, 2, 2, ButtonX + 2 * Screen.IconSize, OffsetY, intBK, Control::Type::ABILITY));
@@ -5079,11 +5205,11 @@ namespace Interface
                 {
                     Interface::CharacterSheet(Renderer, Party.Members[Character], Fonts::Normal, WindowTextWidth, ButtonX, TextY, intWH, true);
                 }
-                else if (Controls[Current].Type == Control::Type::ITEMS)
+                else if (Controls[Current].Type == Control::Type::ITEMS && Equipment.size() > 0)
                 {
-                    for (auto i = 0; i < Party.Members[Character].Equipment.size(); i++)
+                    for (auto i = 0; i < Equipment.size(); i++)
                     {
-                        Graphics::PutText(Renderer, Party.Members[Character].Equipment[i].String().c_str(), Fonts::Normal, 0, clrGR, intWH, TTF_STYLE_NORMAL, ColumnWidth, FontSize, i < 5 ? ButtonX : MidColumn, TextY + 2 * FontSize + (i < 5 ? i : i - 5) * FontSize);
+                        Graphics::PutText(Renderer, Equipment[i].String().c_str(), Fonts::Normal, 0, clrGR, intWH, TTF_STYLE_NORMAL, ColumnWidth, FontSize, i < 5 ? ButtonX : MidColumn, TextY + 2 * FontSize + (i < 5 ? i : i - 5) * FontSize);
                     }
                 }
                 else
@@ -5113,7 +5239,7 @@ namespace Interface
                 }
                 if (Controls[Current].Type == Control::Type::ITEMS && !Hold)
                 {
-                    if (!Party.Members[Character].Equipment.empty())
+                    if (!Equipment.empty())
                     {
                         Interface::ItemScreen(Window, Renderer, StoryScreen, Party, Story, Screen, Text, Offset, Character, Equipment::Mode::USE);
                     }
@@ -5196,7 +5322,7 @@ namespace Interface
         return Result;
     }
 
-    std::vector<Button> CreateChoices(SDL_Window *Window, SDL_Renderer *Renderer, std::vector<Choice::Base> Choices, ScreenDimensions &Screen, int Start, int Last, int Limit, SDL_Color Fg, Uint32 Bg, Uint32 Highlight)
+    std::vector<Button> CreateChoices(SDL_Window *Window, SDL_Renderer *Renderer, std::vector<Choice::Base> &Choices, ScreenDimensions &Screen, int Start, int Last, int Limit, SDL_Color Fg, Uint32 Bg, Uint32 Highlight)
     {
         auto FontSize = TTF_FontHeight(Fonts::Normal);
 
