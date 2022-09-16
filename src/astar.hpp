6f860b2c97122e82554cf1d53916842fc7fd0898
Dael <dael.separa@gmail.com>
2022-09-16 20:44:16 +0800

    use const_iterator

--- src/astar.hpp
+++ src/astar.hpp
@@ -18,7 +18,7 @@ namespace AStar
     template <typename T>
     using Smart = std::shared_ptr<T>;
 
-    template <typename T, typename R = typename std::vector<T>::iterator>
+    template <typename T, typename R = typename std::vector<T>::const_iterator>
     R Find(std::vector<T> &vector, T &key, bool F(T &, T &))
     {
         return std::find_if(vector.begin(), vector.end(), [&](T &f)
@@ -180,7 +180,7 @@ namespace AStar
     }
 
     // Get index of node from a list
-    Moves::iterator Find(Moves &nodes, Smart<AStar::Node> &node)
+    Moves::const_iterator Find(Moves &nodes, Smart<AStar::Node> &node)
     {
         return AStar::Find(nodes, node, Compare);
     }
